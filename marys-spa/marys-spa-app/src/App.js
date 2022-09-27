326a18800adb4bc835d32726fd60a8dbd01f28c6
Kevin Schoen <kevschoen83@gmail.com>
2022-09-27 15:12:40 -0500

    Adding backend & frontend of mary's spa application

---
+++ marys-spa/marys-spa-app/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to Mary's Spa!</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ marys-spa/marys-spa/src/main/java/com/example/genspark/marysspa/MarysSpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.genspark.marysspa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarysSpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarysSpaApplication.class, args);
+	}
+
+}
